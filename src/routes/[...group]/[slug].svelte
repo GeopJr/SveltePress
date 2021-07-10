c8409b16b1f66363591c6dfdf191ab465bd10dca
Evangelos Paterakis <evan@geopjr.dev>
2021-07-10 16:40:36 +0300

    Revert "fix(slug): set title not reactive"
    This reverts commit 4e28e09517a4d903af2ee68ff6724345b74a5d91.

--- src/routes/[...group]/[slug].svelte
+++ src/routes/[...group]/[slug].svelte
@@ -49,7 +49,7 @@
 	export let post;
 
 	// the post title is either postName if it exists else slug
-	const title = post.meta?.postName ? post.meta.postName : $page.path.split('/').pop();
+	$: title = post.meta?.postName ? post.meta.postName : $page.path.split('/').pop();
 </script>
 
 <svelte:head>
