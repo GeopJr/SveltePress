edb1a89ec45600092d55747e24d1d3f0d8f750b0
Henrique Borges <31735860+henriquehbr@users.noreply.github.com>
2021-10-29 09:05:43 -0300

    refactor: use ternary operator instead if statement

--- src/routes/[...group]/[slug].svelte
+++ src/routes/[...group]/[slug].svelte
@@ -15,17 +15,15 @@
 				if (defaultFile) break;
 			}
 
-			if (!defaultFile) {
-				return {
+			return !defaultFile
+				? {
 					status: 400,
-					error: 'No posts available'
-				};
-			}
-
-			return {
-				status: 307,
-				redirect: defaultFile.replace(/\.[^/.]+$/, '')
-			};
+					error: 'No posts available
+				}
+				: {
+					status: 307,
+					redirect: defaultFile.replace(/\.[^/.]+$/, '')
+				}
 		}
 
 		// needs to be post so slug and group are passed
